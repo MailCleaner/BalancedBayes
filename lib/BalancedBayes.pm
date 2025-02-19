82ded7782ccc05aa65f200ab22f99d0251570baa
John Mertz <git@john.me.tz>
2025-02-19 23:25:20 +0100

    Add in-line documentation and method stubs

--- lib/BalancedBayes.pm
+++ lib/BalancedBayes.pm
@@ -1,6 +1,56 @@
-#!/usr/bin/perl
+=head1 NAME
+
+BalancedBayes - Tools for maintaining a Bayes DB with balanced training data
+
+=cut
 
 package BalancedBayes 0.001;
+
+=head1 DESCRIPTION
+
+This module provides tools to help you maintain a balanced Bayes training database.
+
+It will maintain separate queues for 'False-Postives', 'False-Negatives', 'True-Positives' and
+'True-Negatives', for each detected language. This should help to ensure that there is never a
+disproportionate number of items of one disposition which could poison the filter.
+
+The tool allows for vendor specific functions to be implemented in order to specify how to verify
+and train messages for that product.
+
+=head1 EXPORT
+
+None by default. Initialize an object with C<new> and access all functions as methods.
+
+=head1 SYNOPSIS
+
+  # Load Module
+  use BalancedBayes;
+
+  # Initialize BalancedBayes object and import vendor configuration
+  my $bb = BalancedBayes->new( vendor => 'MailCleaner' );
+
+  # Load a message file
+  $bb->load_message($eml_file);
+
+  # Validate the message and feed to bayes. Here we adjust for a false-positive:
+  die "$eml_file was not scranned by MailCleaner\n" unless $bb->validate_vendor();
+  die "$eml_file was not detected as Spam" unless $bb->validate_spam();
+  $bb->false_positive();
+
+  # Run queues
+  $bb->process_queues();
+
+=head1 DEPENDENCIES
+
+  Depends on CPAN distributions:
+
+  Cwd
+  File::MimeInfo
+  File::Slurp
+
+  See child modules for additional specific dependencies.
+
+=cut
 
 use strict;
 use warnings;
@@ -8,6 +58,20 @@ use warnings;
 use Cwd;
 use File::MimeInfo;
 use File::Slurp;
+
+=head1 METHODS
+
+=head2 new
+
+Initialize an object.
+
+  my $bb = Balanced::Bayes->new( vendor => 'MailCleaner' );
+
+Vendor settings can also be included in argument hash to override defaults:
+
+  my $bb = Balanced::Bayes->new( vendor => 'MailCleaner', rootdir => '/tmp/BalancedBayes' );
+
+=cut 
 
 sub new {
 	my $class = shift || "BalancedBayes";
@@ -17,7 +81,7 @@ sub new {
 	};
 	delete($args{vendor}) if defined($args{vendor});
 	bless($this, $class);
-	$this->load_vendor($this->{vendor});
+	$this->_load_vendor($this->{vendor});
 	foreach my $key (keys(%args)) {
 		$this->{vendor}->{$key} = $args{$key};
 	}
@@ -26,7 +90,13 @@ sub new {
 	return $this;
 }
 
-sub load_vendor {
+=head2 _load_vendor
+
+Internal function to import methods and settings from defined vendor.
+
+=cut
+
+sub _load_vendor {
 	my ($this, $vendor) = @_;
 	my $pkg = "BalancedBayes::Vendor::$vendor";
 	eval "use $pkg";
@@ -37,25 +107,55 @@ sub load_vendor {
 	$this->{vendor}->defaults();
 }
 
+=head2 rootdir
+
+Getter/setter for rootdir setting.
+
+=cut
+
 sub rootdir {
 	my ($this, @args) = @_;
 	return $this->{vendor}->rootdir(@args);
 }
+
+=head2 recipient
+
+Getter/setter for email recipient setting.
+
+=cut
 
 sub recipient {
 	my ($this, @args) = @_;
 	return $this->{vendor}->recipient(@args);
 }
 
+=head2 sender
+
+Getter/setter for email sender setting.
+
+=cut
+
 sub sender {
 	my ($this, @args) = @_;
 	return $this->{vendor}->sender(@args);
 }
 
+=head2 smtp_server
+
+Getter/setter for email smtp_server setting.
+
+=cut
+
 sub smtp_server {
 	my ($this, @args) = @_;
 	return $this->{vendor}->smtp_server(@args);
 }
+
+=head2 load_message
+
+Accepts a path to an email file and loads it in as the working message.
+
+=cut
 
 sub load_message {
 	my $this = shift;
@@ -64,23 +164,116 @@ sub load_message {
 	$this->{msg} = $raw;
 }
 
-sub detect_mc {
+=head2 validate_vendor
+
+Request passed to Vendor method. Returns true/false for whether the message should be processed for
+that vendor. ie. Don't train MailCleaner with messages which were not already scanned previously
+by MailCleaner.
+
+Using this method is optional, but should be used for corrective training (ie. False-Positive/
+False-Negative reports), not necessarily reinforcement training (ie. known Spam or Ham from
+external sources).
+ 
+=cut
+
+sub validate_vendor {
 	my $this = shift;
 	my $raw = shift;
 	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->validate_vendor();
 }
 
-sub detect_nicebayes {
+=head2 validate_ham
+
+Request passed to Vendor method. Returns true/false for whether the message was previously detected
+as spam by the vendor.
+
+This should be used after C<validate_vendor> has been confirmed and is only necessary for corrective
+training.
+
+=cut
+
+sub validate_ham {
 	my $this = shift;
 	my $raw = shift;
 	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->validate_spam();
 }
 
-sub detect_bogospam {
+=head2 validate_spam
+
+Request passed to Vendor method. Returns true/false for whether the message was previously detected
+as spam by the vendor.
+
+This should be used after C<validate_vendor> has been confirmed and is only necessary for corrective
+training.
+
+=cut
+
+sub validate_spam {
 	my $this = shift;
 	my $raw = shift;
 	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->validate_spam();
 }
+
+=head2 false_negative
+
+Request passed to Vendor method. Add message to relevant False-Negative queue.
+
+=cut
+
+sub false_negavite {
+	my $this = shift;
+	my $raw = shift;
+	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->false_negative();
+}
+
+=head2 false_positive
+
+Request passed to Vendor method. Add message to relevant True-Positive queue.
+
+=cut
+
+sub false_positive {
+	my $this = shift;
+	my $raw = shift;
+	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->false_positive();
+}
+
+=head2 true_negative
+
+Request passed to Vendor method. Add message to relevant True-Negative queue.
+
+=cut
+
+sub true_negative {
+	my $this = shift;
+	my $raw = shift;
+	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->true_negative();
+}
+
+=head2 true_positive
+
+Request passed to Vendor method. Add message to relevant True-Positive queue.
+
+=cut
+
+sub true_positive {
+	my $this = shift;
+	my $raw = shift;
+	$this->load_message($raw) if (defined($raw));
+	return $this->{vendor}->true_positive();
+}
+
+=head2 check_dir
+
+Verify that the queue directory (or others) exist and create it if it does not.
+
+=cut
 
 sub check_dir {
 	my ($this, $dir) = @_;
@@ -97,6 +290,14 @@ sub check_dir {
 		}
 	}
 }
+
+=head2 queue_report
+
+Generate a report with the number of items currently available in each queue and the number of
+messages which can potentially be processed at this time as well as giving an indication of which
+message types are needed to do additional training.
+
+=cut
 
 sub queue_report {
 	my ($this, $email) = @_;
@@ -135,11 +336,17 @@ sub queue_report {
 	if (defined($email)) {
 		print "Send email to $email\n";
 	} else {
-		$this->send_report($this->{recipient}, 'Balanced Bayes queue report', $report);
+		$this->_send_report($this->{recipient}, 'Balanced Bayes queue report', $report);
 	}
 }
 
-sub send_report {
+=head2 _send_report
+
+Internal method. Email generated report to configured recipient.
+
+=cut
+
+sub _send_report {
 	my ($this, $to, $subject, $body, @attach) = @_;
 	use MIME::Entity;
 	use Net::SMTP;
@@ -189,5 +396,55 @@ sub send_report {
 		print "Status unknown: ".$smtp->message()."\n";
 	}
 }
+
+=head1 CHILD MODULES
+
+=over 8
+
+=item BalancedBayes::Vendor::Stub
+
+A stub file which exists to be copied and modified to simplify the creation of a new Vendor module.
+
+=item BalancedBayes::Vendor::MailCleaner
+
+Vendor-specific functionality for MailCleaner
+
+=item BalancedBayes::Languages
+
+A module to detect the primary language of an input email for sorting into the proper queue.
+
+=back
+
+=head1 HISTORY
+
+=over 8
+
+=item 0.01
+
+Initial release with enough functionality to train NiceBayes and SpamC Bogospam
+
+=back
+
+=head1 SEE ALSO
+
+Github repository for issues and contributions:
+
+https://github.com/MailCleaner/BalancedBayes
+
+=head1 AUTHOR
+
+John Mertz <git@john.me.tz>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2025 by Alinto AG
+
+=head1 LICENSE
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+General Public License version 3 or later. This license is included in full with this software
+distribution and must be included with any modifications or redistributions.
+
+=cut
 
 1;
--- lib/BalancedBayes/Vendor/MailCleaner.pm
+++ lib/BalancedBayes/Vendor/MailCleaner.pm
@@ -1,8 +1,48 @@
+=head1 NAME
+
+BalancedBayes::Vendor::MailCleaner - Vender-specific settings and functions for MailCleaner
+
+=cut
+
 package BalancedBayes::Vendor::MailCleaner 0.001;
+
+=head1 DESCRIPTION
+
+This module provides vendor-specific settings and functions for use by BalancedBayes with
+MailCleaner.
+
+=head1 EXPORT
+
+None by default. Initialize a BalancedBayes object with C<new> and the MailCleaner vendor and
+access these functions by calling the exposed functions directly in that object.
+
+You may have some legitimate reason export C<detect_nicebayes>, C<detect_bogospam>,
+C<feed_nicebayes>, and C<feed_bogospam> within your script if you would like to train one
+but not the other.
+
+=head1 SYNOPSIS
+
+  # Import MailCleaner vendor configuration when initializing BalancedBayes object
+  my $bb = BalancedBayes->new( vendor => 'MailCleaner' );
+
+=head1 DEPENDENCIES
+
+  Depends on BalancedBayes::Vendor base module.
+
+=cut
+
 use strict;
 use warnings;
 
 use base "BalancedBayes::Vendor";
+
+=head1 METHODS
+
+=head2 defaults
+
+Load default settings for MailCleaner vendor.
+
+=cut
 
 sub defaults {
 	my ($this) = @_;
@@ -10,6 +50,69 @@ sub defaults {
 	$this->{sender} = 'root@gate-pp1.maillcleaner.net';
 	$this->{recipient} = 'support@mailcleaner.net';
 	$this->{smtp_server} = 'localhost:2525';
+}
+
+=head2 validate_vendor
+
+Verify that the working message was previously scanned by MailCleaner.
+
+=cut
+
+sub validate_vendor {
+	my $this = shift;
+	#TODO find X-MailCleaner header
+	return 0;
+	return 1;
+}
+
+=head2 validate_ham
+
+Verify that the working message was actually detected as a ham previously by MailCleaner.
+
+=cut
+
+sub validate_ham {
+	my $this = shift;
+	return 0 if ($this->detect_nicebayes || $this->detect_bogospam);
+	return 1;
+}
+
+=head2 validate_ham
+
+Verify that the working message was actually detected as a spam previously by MailCleaner.
+
+=cut
+
+sub validate_spam {
+	my $this = shift;
+	return 1 if ($this->detect_nicebayes || $this->detect_bogospam);
+	return 0;
+}
+
+=head2 detect_nicebayes
+
+Verify that the working message was actually detected as a spam by NiceBayes.
+
+=cut
+
+sub detect_nicebayes {
+	my $this = shift;
+	my $raw = shift;
+	#TODO find NiceBayes result in X-SpamCheck header
+	$this->load_message($raw) if (defined($raw));
+}
+
+=head2 detect_bogospam
+
+Verify that the working message was actually detected as a spam by Bogospam.
+
+=cut
+
+sub detect_bogospam {
+	my $this = shift;
+	my $raw = shift;
+	#TODO find BAYES_* rules in SpamC results in X-SpamCheck header
+	$this->load_message($raw) if (defined($raw));
 }
 
 1;
